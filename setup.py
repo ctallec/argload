2dab3627f299f78afa0635e04b7d8e1747386790
ctallec <corentinxtallec@gmail.com>
2018-03-26 17:19:05 +0200

    [FIX] Setup modified

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from setuptools import setup
 setup(name='argload',
       version='0.1',
       description='Argparse, easy reloading of command line arguments',
-      url='https://github.com/ctallec/argload.git',
+      url='https://github.com/ctallec/argload',
+      download_url='https://github.com/ctallec/argload/archive/0.1.tar.gz',
       author='Corentin Tallec, Thomas Lucas',
       author_email='corentin.tallec@polytechnique.edu',
       license='MIT',
       packages=['argload'],
+      keywords=['command-line-parser', 'hyperparameters'],
       zip_safe=False)
