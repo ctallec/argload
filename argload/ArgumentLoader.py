33b7a77a98a1b8a7649ca67b6d09184d85f0be21
ctallec <corentinxtallec@gmail.com>
2018-03-26 18:37:31 +0200

    [FIX] Ill behavior with default arguments (still not clean)

--- argload/ArgumentLoader.py
+++ argload/ArgumentLoader.py
@@ -90,7 +90,8 @@ class ArgumentLoader(object):
     def _fuse_args(self, dumped_args, args, overwrite):
         fused_args = {}
         for k in list(dumped_args.keys()) + list(args.keys()):
-            if k in dumped_args and k in args and dumped_args[k] != args[k]:
+            if k in dumped_args and k in args and dumped_args[k] != args[k] and\
+                    args[k] != self._parser.get_default(k):
                 if overwrite:
                     fused_args[k] = args[k]
                 elif args[k] == self._parser.get_default(k):
