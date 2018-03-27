eabc116f58769c81365ee54aa0cd891c62c2b5ad
ctallec <corentinxtallec@gmail.com>
2018-03-27 09:46:11 +0200

    [FIX] Adapt setup.py to tag file

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup
 
 setup(name='argload',
-      version='0.1.1',
+      version='0.1.2',
       description='Argparse, easy reloading of command line arguments',
       url='https://github.com/ctallec/argload',
-      download_url='https://github.com/ctallec/argload/archive/0.1.1.tar.gz',
+      download_url='https://github.com/ctallec/argload/archive/0.1.2.tar.gz',
       author='Corentin Tallec, Thomas Lucas',
       author_email='corentin.tallec@polytechnique.edu',
       license='MIT',
